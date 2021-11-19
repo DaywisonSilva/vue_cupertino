bd576385072491610153489d1b1f8f0c908dd166
DaywisonSilva <daywisonsilvasou@gmail.com>
2021-11-19 20:21:17 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import 'onsenui/css/onsenui.css'
+import 'onsenui/css/onsen-css-components.css'
+import VueOnsen from 'vue-onsenui'
 
-Vue.config.productionTip = false;
+Vue.use(VueOnsen)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app')
---
+++ src/pages/Page1.vue
@@ -0,0 +1,22 @@
+<template id="page1">
+  <v-ons-page>
+    <v-ons-toolbar>
+      <div class="center">Page 1</div>
+    </v-ons-toolbar>
+    <p style="text-align: center">
+      This is the first page
+      <v-ons-button @click="push">Push Page 2</v-ons-button>
+    </p>
+  </v-ons-page>
+</template>
+
+<script>
+import page2 from '@/pages/Page2.vue'
+export default {
+  methods: {
+    push() {
+      this.$emit('push-page', page2)
+    },
+  },
+}
+</script>
